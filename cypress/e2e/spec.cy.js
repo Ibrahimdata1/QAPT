4fffc7a1372e48370f4c5351d64a1c64ecd11598
Ibrahimdata1 <bsk.kosit1@gmail.com>
2026-01-30 01:55:02 +0700

    add typing parrot step before search

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,10 @@
 describe('Mission check parrot img', () => {
   it('search parrot and click first parrot pic', () => {
-    cy.visit('https://www.google.com/search?q=parrot&udm=2')
+    cy.visit('https://www.google.com/')
+    //type parrot on search box
+    cy.get('textarea[name="q"]').type('parrot{enter}')
+    //click image tab and test 
+    cy.contains('a',/รูปภาพ|Images/).should('be.visible').click()
     //test url
     cy.url().should('include','udm=2')
     //get first parrot pic
